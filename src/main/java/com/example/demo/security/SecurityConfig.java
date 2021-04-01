fa7db30b9640d18bdf683710af02db30fb4d42f0
soohyunnn <sksmsqodn20@gmail.com>
2021-04-01 23:51:05 +0900

    [BE]로그아웃 처리 필터

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/**")  //모든 요청을 다 처리할 것데
                 .authorizeRequests()  //요청을 어떻게 인가할지 아래에 설정하는 과정이다. (http.authorizeRequests() : 요청의 인가 설정은...)
-                .mvcMatchers("/", "/info", "/account/**").permitAll()  //루트 경로는 모든 사용자 접근 가능
+                .mvcMatchers("/", "/info", "/account/**", "/signup").permitAll()  //루트 경로는 모든 사용자 접근 가능
                 .mvcMatchers("/admin").hasRole("ADMIN")  // '/admin' 경로는 권한이 ADMIN인 사용자만 접근 가능
                 .mvcMatchers("/user").hasRole("USER")   //'/user' 경로는 권한이 USER인 사용자만 접근 가능
                 .anyRequest().authenticated()    //anyRequest()는 기타 등등을 의미, 기타 등등에 대한 접근은 인증만 하면 가능
@@ -53,6 +53,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin()  //그리고 formLogin 사용
                 .and()
                 .httpBasic(); //그리고 httoBasic 사용
+
+        http.logout().logoutSuccessUrl("/");
+//                .logoutUrl("/logout")  //logout을 처리하는 url(기본으로 제공해주는 로그아웃 페이지가 /logout 임. 다른 url로 바꾸려면 새로 커스터마이징 한 페이지를 생성해야 합니다.
+//                  //logout이 성공하면 이동할 url
+//                .invalidateHttpSession(true)  //로그아웃 한 후 HttpSession을 invalid 처리 할 것인가(기본값이 true)
+//                .deleteCookies();  //쿠키 기반의 인증 방법을 사용할 경우 로그아웃 한 후 쿠키를 삭제해주는 역할을 함.
+
+
 
         //기본값은 동일한 Thread내에서만 공유됨.
         //현재 Thread에서 하위 Thread로 SecurityContext가 공유가 됨.
